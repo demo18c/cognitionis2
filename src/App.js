34d6236b2d11c3316c3d2eccdc53ce36a4e60da2
RASHAD ABDULBARR <rashadabdulbarr@RASHADs-MacBook-Pro.local>
2021-05-04 12:43:52 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-import Radio from './Radio';
-
+import React, { Suspense } from 'react';
 import './App.css';
+//
+// import Amplify from 'aws-amplify';
+// import awsExports from './aws-exports';
+// import { withAuthenticator } from '@aws-amplify/ui-react';
+//
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import { Route, Switch } from 'wouter';
+
+import Sidebar from './components/Sidebar';
+
+import Dashboard from './pages/Dashboard';
+import RadioSpec from './pages/radio/RadioSpec';
+import Overview from './pages/Overview';
+import AboutRadio from './pages/radio/AboutRadio';
+import Radio from './pages/radio/Radio';
+import RadioTrouble from './pages/radio/RadioTrouble';
+
+// Amplify.configure(awsExports);
 
 function App() {
 	return (
 		<Router>
-			<Switch>
-				<Route path="/radio" component={Radio} />
-			</Switch>
+			<Sidebar />
+			<Suspense fallback={<div>Loading...</div>}>
+				<Switch>
+					<Route exact path="/" component={Dashboard} />
+					<Route path="/overview" component={Overview} />
+					<Route path="/aboutradio" component={AboutRadio} />
+					<Route path="/radio" component={Radio} />
+					<Route path="/radiospec" component={RadioSpec} />
+					<Route path="/radiotrouble" component={RadioTrouble} />
+				</Switch>
+			</Suspense>
 		</Router>
 	);
 }
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+	return (
+		<div>
+			<h1 className="dashh1">cognitionis</h1>
+		</div>
+	);
+};
+
+export default Dashboard;
